35b6b88dc9f6d644cf68208e87d61ef984235641
Tanner Stratford <57194030+tannerstratford@users.noreply.github.com>
2021-05-14 01:08:21 -0600

    fixed error

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const cors = require('cors');
+const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localhost:5000
 
 const express = require('express');
 const bodyParser = require('body-parser');
